2fff563594425002e6fa67453484968b3d833d8f
revanthkumarJ <146642552+revanthkumarJ@users.noreply.github.com>
2024-02-01 18:58:06 +0530

    Time: 7 ms (85.03%) | Memory: 44.9 MB (21.43%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,17 +1,5 @@
 class Solution {
-    public int find(int m,int n,String s1,String s2)
-    {
-        if(m==0)
-        return n;
-        if(n==0)
-        return m;
-        if(s1.charAt(m-1)==s2.charAt(n-1))
-        return find(m-1,n-1,s1,s2);
-        else
-        return 1+(int)Math.min(find(m-1,n,s1,s2),find(m,n-1,s1,s2));
-    }
     public int minDistance(String word1, String word2) {
-        //return find(word1.length(),word2.length(),word1,word2);
         int m=word1.length(),n=word2.length();
         int array[][]=new int[m+1][n+1];
         for(int i=0;i<=m;i++)
