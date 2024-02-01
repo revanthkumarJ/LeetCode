2553ff17b416fa3ac05ee07c29f89a9ddf613d87
revanthkumarJ <146642552+revanthkumarJ@users.noreply.github.com>
2024-02-01 18:46:22 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (86.90%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,18 +1,6 @@
 class Solution {
     
     public boolean stoneGame(int[] piles) {
-        int a=0;
-        int b=0;
-        int res=1,ln=piles.length;
-        for(int i=0;i<ln;i++){
-            if(res%2!=0){
-                a+=Math.max(piles[i],piles[ln-i-1]);
-            }
-            else{
-                b+=Math.min(piles[i],piles[ln-i-1]);
-            }
-            res++;
-        }
-        return a>b;
+        return true;
     }
 }
