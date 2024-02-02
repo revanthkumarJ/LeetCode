5993431aeb8838d624cae30b02aff77e5d23fdb3
revanthkumarJ <146642552+revanthkumarJ@users.noreply.github.com>
2024-02-02 18:49:27 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (58.91%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,56 +1,34 @@
 class Solution {
-    public String concat(String s,int n)
+    public String decodeString(String s) 
     {
-        StringBuilder r=new StringBuilder(s);
-        for(int i=2;i<=n;i++)
+        StringBuilder r=new StringBuilder("");
+        Stack<Integer> num=new Stack<>();
+        Stack<StringBuilder> st=new Stack<>();
+        int n=0;
+        for(char c:s.toCharArray())
         {
-            r.append(s);
+            if(Character.isDigit(c))
+                n=n*10+(c-'0');
+            else if(c=='[')
+            {
+                num.push(n);
+                n=0;
+                st.push(r);
+                r=new StringBuilder("");
+            }
+            else if(c==']')
+            {
+                StringBuilder temp=r;
+                r=st.pop();
+                int k=num.pop();
+                while(k-->0)
+                {
+                    r.append(temp);
+                }
+            }
+            else
+            r.append(c);
         }
         return r.toString();
-
-    }
-    public String decodeString(String s) {
-        int left=0,right=0,l=-1,r=0;
-        int n=s.length();
-        do{
-            l=-1;
-            left=0;
-            right=0;
-            int num=0;
-            int numStart=-1;
-            for(int i=0;i<s.length();i++)
-            {
-                char d=s.charAt(i);
-                if(d=='[')
-                {
-                    if(l==-1)
-                    l=i;
-                    left++;
-                }
-                else if(d==']')
-                {
-                    right++;
-                    if(left==right)
-                    {
-                        r=i;
-                        break;
-                    }
-                }
-                else if(d>='0' && d<='9')
-                {
-                    if(numStart==-1)
-                    numStart=i;
-                    if(l==-1)
-                        num=num*10+(d-'0');
-                } 
-            }
-            if(l==-1)
-            break;
-            if(r+1<s.length())
-            s=s.substring(0,numStart)+concat(s.substring(l+1,r),num)+s.substring(r+1);
-            else
-            s=s.substring(0,numStart)+concat(s.substring(l+1,r),num);
-        }while(l!=-1);
-        return s;
     }
 }
