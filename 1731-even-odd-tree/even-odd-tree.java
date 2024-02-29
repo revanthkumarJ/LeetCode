764a6a958cf5a9819a9e600c977e89e1e4fe34cb
JILAKARA REVANTH KUMAR <146642552+revanthkumarJ@users.noreply.github.com>
2024-02-29 08:24:07 +0530

    Time: 13 ms (50.59%) | Memory: 66.3 MB (38.87%) - LeetSync

--- 1731-even-odd-tree/even-odd-tree.java
+++ 1731-even-odd-tree/even-odd-tree.java
@@ -1,18 +1,3 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean isEvenOddTree(TreeNode root) {
         boolean isEven=true;
@@ -22,32 +7,30 @@ class Solution {
         {
             int count=q.size();
             int prev=-1;
-    
-                for(int i=1;i<=count;i++)
+            for(int i=1;i<=count;i++)
+            {
+                TreeNode ele=q.poll();
+                if( isEven)
                 {
-                    TreeNode ele=q.poll();
-                    if( isEven)
-                    {
-                        if(ele.val%2==0)
+                    if(ele.val%2==0)
                         return false;
-                        if(!(prev==-1 || prev<ele.val))
+                    if(!(prev==-1 || prev<ele.val))
                         return false;
-                    }
-                    else
-                    {
-                        if(ele.val%2!=0)
-                        return false;
-                        if(!(prev==-1 || prev>ele.val))
-                        return false;
-                    }
-                    if(ele.left!=null)
-                    q.add(ele.left);
-                    if(ele.right!=null)
-                    q.add(ele.right);
-                    prev=ele.val;
                 }
+                else
+                {
+                    if(ele.val%2!=0)
+                        return false;
+                    if(!(prev==-1 || prev>ele.val))
+                        return false;
+                }
+                if(ele.left!=null)
+                    q.add(ele.left);
+                if(ele.right!=null)
+                    q.add(ele.right);
+                prev=ele.val;
+            }
             isEven=!isEven;
-            
         }
         return true;
     }
