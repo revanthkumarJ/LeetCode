8e497679d4276457374ccbb7976a5f15f213c618
JILAKARA REVANTH KUMAR <146642552+revanthkumarJ@users.noreply.github.com>
2024-03-01 06:37:08 +0530

    Time: 38 ms (59.91%) | Memory: 16.5 MB (75.75%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.py
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.py
@@ -4,9 +4,4 @@ class Solution:
         for i in s:
             if(i=='0'):
                 o=o+1
-        r="1"
-        for i in range(0,o):
-            r="0"+r
-        for i in range(len(r),len(s)):
-            r="1"+r
-        return r
+        return ("1"*(len(s)-o-1))+("0"*o)+"1"
