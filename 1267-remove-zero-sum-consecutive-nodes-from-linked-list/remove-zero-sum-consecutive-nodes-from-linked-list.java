1c43baa2b350a48c0f89468ab770946293bcdd12
JILAKARA REVANTH KUMAR <146642552+revanthkumarJ@users.noreply.github.com>
2024-03-12 08:25:26 +0530

    Time: 3 ms (54.35%) | Memory: 44.4 MB (18.97%) - LeetSync

--- 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.java
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.java
@@ -1,38 +1,33 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode removeZeroSumSublists(ListNode head) {
-        int prefixSum = 0;
-        HashMap<Integer, ListNode> map = new HashMap<>();
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        map.put(0, dummy);
-        while (head != null) {
-            prefixSum += head.val;
-            if (map.containsKey(prefixSum)) {
-                ListNode start = map.get(prefixSum);
-                ListNode temp = start;
-                int sum = prefixSum;
-                while (temp != head) {
-                    temp = temp.next;
-                    sum += temp.val;
-                    if (temp != head)
-                        map.remove(sum);
+class Solution 
+{
+    public ListNode removeZeroSumSublists(ListNode head) 
+    {
+        int prefixSum=0;
+        ListNode n=new ListNode(0);
+        HashMap<Integer,ListNode> array=new HashMap<>();
+        array.put(0,n);
+        n.next=head;
+        while(head!=null)
+        {
+            prefixSum+=head.val;
+            if(array.containsKey(prefixSum))
+            {
+                ListNode start=array.get(prefixSum);
+                int sum=prefixSum;
+                ListNode temp=start.next;
+                while(temp!=head)
+                {
+                    sum+=temp.val;
+                    array.remove(sum);
+                    temp=temp.next;
+                    
                 }
-                start.next = head.next;
-            } else {
-                map.put(prefixSum, head);
+                start.next=head.next;
             }
-            head = head.next;
+            else
+                array.put(prefixSum,head);
+            head=head.next;
         }
-        return dummy.next;
+        return n.next;
     }
 }
