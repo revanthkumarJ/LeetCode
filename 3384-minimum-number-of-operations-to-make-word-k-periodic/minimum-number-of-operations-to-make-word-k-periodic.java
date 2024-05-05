741d72aea364a96fc5562d5dae68ea773f816e73
JILAKARA REVANTH KUMAR <146642552+revanthkumarJ@users.noreply.github.com>
2024-05-05 10:22:19 +0530

    Time: 39 ms (22.22%) | Memory: 45.4 MB (44.44%) - LeetSync

--- 3384-minimum-number-of-operations-to-make-word-k-periodic/minimum-number-of-operations-to-make-word-k-periodic.java
+++ 3384-minimum-number-of-operations-to-make-word-k-periodic/minimum-number-of-operations-to-make-word-k-periodic.java
@@ -1,15 +1,13 @@
 class Solution {
     public int minimumOperationsToMakeKPeriodic(String word, int k) {
-        String s=null;
-        int maxFreq=0;
         HashMap<String,Integer> map=new HashMap<>();
+        int maxFreq=0;
         for(int i=0;i<word.length();i=i+k)
         {
             String r=word.substring(i,i+k);
             map.put(r,map.getOrDefault(r,0)+1);
             if(map.get(r)>maxFreq)
             {
-                s=r;
                 maxFreq=map.get(r);
             }
         }
