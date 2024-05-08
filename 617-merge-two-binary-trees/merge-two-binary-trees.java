83350e2659c4b68f456bf55e68e643a8e112c574
JILAKARA REVANTH KUMAR <146642552+revanthkumarJ@users.noreply.github.com>
2024-05-08 09:52:47 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (83.94%) - LeetSync

--- 617-merge-two-binary-trees/merge-two-binary-trees.java
+++ 617-merge-two-binary-trees/merge-two-binary-trees.java
@@ -19,22 +19,16 @@ class Solution {
         if(r1!=null && r2!=null)
         {
             r1.val+=r2.val;
+
             if(r1.left==null && r2.left!=null) 
-            {
                 r1.left=r2.left;
-            }
             else if(r1.left!=null && r2.left!=null)
-            {
                 merge(r1.left,r2.left);
-            }
+
             if(r1.right==null && r2.right!=null)
-            {
                 r1.right=r2.right;
-            }
             else if(r1.right!=null && r2.right!=null)
-            {
                 merge(r1.right,r2.right);
-            }
         }
     }
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
