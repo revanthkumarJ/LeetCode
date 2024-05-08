20f0726bff3459486b72da4a00a6c471d2c1a259
JILAKARA REVANTH KUMAR <146642552+revanthkumarJ@users.noreply.github.com>
2024-05-08 10:09:03 +0530

    Time: 16 ms (87.74%) | Memory: 46.1 MB (48.57%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
@@ -1,35 +1,37 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    List<Integer> res;
-    public void add(TreeNode root)
-    {
-        if(root!=null)
-        {
-            add(root.left);
-            res.add(root.val);
-        add(root.right);
-        }
-        
-    }
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        res=new ArrayList<>();
-        add(root1);
-        add(root2);
-        Collections.sort(res);
-        return res;
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        inorder(root1,list1);
+        inorder(root2,list2);
+        List<Integer> ans = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while(i<list1.size()&&j<list2.size()){
+            if(list1.get(i)>=list2.get(j)){
+                ans.add(list2.get(j));
+                j++;
+            }
+            else{
+                ans.add(list1.get(i));
+                i++;
+            }
+        }
+        while(i<list1.size()){
+            ans.add(list1.get(i));
+            i++;
+        }
+        while(j<list2.size()){
+            ans.add(list2.get(j));
+            j++;
+        }
+        return ans;
+    }
+    public void inorder(TreeNode node,List<Integer> list){
+        if(node==null)
+            return;
+        inorder(node.left,list);
+        list.add(node.val);
+        inorder(node.right,list);
     }
 }
